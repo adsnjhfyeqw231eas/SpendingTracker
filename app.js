d8656221fbb5625ca7169a8af1800831a1492176
Shubham Deshmukh <dshubham1295@gmail.com>
2023-06-12 12:15:47 +0530

    change for vercel deployment

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 var express = require("express");
 var app = express();
-var morgan = require('morgan');
+var morgan = require("morgan");
 var bodyParser = require("body-parser");
-require('custom-env').env()
+require("custom-env").env();
 //MongoDb Connection
-require('./models/db.js');
+require("./models/db.js");
 
-var index = require('./routes/index');
-var date = require('./routes/date');
-var setting = require('./routes/setting');
-
+var index = require("./routes/index");
+var date = require("./routes/date");
+var setting = require("./routes/setting");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.use('/', index);
-app.use('/date', date);
-app.use('/setting', setting);
+app.use("/", index);
+app.use("/date", date);
+app.use("/setting", setting);
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,162 +1,201 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Sugar = require('sugar');
-const Spending = require('../models/spending');
-const Category = require('../models/category');
+const mongoose = require("mongoose");
+const Sugar = require("sugar");
+const Spending = require("../models/spending");
+const Category = require("../models/category");
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
-
 router.get("/", (req, res, next) => {
+  const currentDate = new Date();
 
-    const currentDate = new Date();
+  GetQuery(Spending, function (result, test) {
+    var totalExp = formatMoney(result[0].total);
 
-    GetQuery(Spending, function (result, test) {
-        var totalExp = formatMoney(result[0].total);
-
-        try {
-            Category.find({}, { _id: 0, description: 0 })
-                .then(out => {
-                    res.status(200).render('index', {
-                        total: totalExp,
-                        item: test,
-                        month: monthNames[currentDate.getMonth()],
-                        year: currentDate.getFullYear(),
-                        category: out
-                    });
-                }).catch(err => {
-                    throw new Error(err);
-                });
-        } catch (error) {
-            console.log(error);
-        }
-
-    });
-
+    try {
+      Category.find({}, { _id: 0, description: 0 })
+        .then((out) => {
+          res.status(200).render("index", {
+            total: totalExp,
+            item: test,
+            month: monthNames[currentDate.getMonth()],
+            year: currentDate.getFullYear(),
+            category: out,
+          });
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
 
 router.post("/", (req, res, next) => {
-    var bodyDate = GetFormattedDate(req.body.date);
-    const spend = new Spending({
-        _id: new mongoose.Types.ObjectId(),
-        item: req.body.item,
-        date: bodyDate,
-        cost: req.body.cost,
-        category: req.body.category
+  var bodyDate = GetFormattedDate(req.body.date);
+  const spend = new Spending({
+    _id: new mongoose.Types.ObjectId(),
+    item: req.body.item,
+    date: bodyDate,
+    cost: req.body.cost,
+    category: req.body.category,
+  });
+
+  spend
+    .save()
+    .then((result) => {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ result }, null, 3));
+    })
+    .catch((err) => {
+      console.log(err);
     });
-
-    spend.save().then(result => {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ result }, null, 3));
-    }).catch(err => {
-        console.log(err);
-    });
-
-
 });
 
 router.get("/price", (req, res, next) => {
-    GetQuery(Spending, function (result, test) {
-        var totalExp = formatMoney(result[0].total);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ totalExp, test }, null, 3));
+  GetQuery(Spending, function (result, test) {
+    var totalExp = formatMoney(result[0].total);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ totalExp, test }, null, 3));
+  });
+});
+
+router.get("/search_item/:category", (req, res, next) => {
+  Spending.distinct("item", { category: req.params.category })
+    .then((result) => {
+      res.status(200).json({
+        message: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
-router.get('/search_item/:category', (req, res, next) => {
-
-    Spending.distinct("item", { category: req.params.category }).then(result => {
-        res.status(200).json({
-            message: result
-        });
-    }).catch(err => {
-        console.log(err);
-    });
-});
-
-router.get('/graph', (req, res, next) => {
-    res.status(200).render('monthlyGraph');
+router.get("/graph", (req, res, next) => {
+  res.status(200).render("monthlyGraph");
 });
 
 module.exports = router;
 
-function GetFormattedDate (date) {
-    var fields = date.split('/');
+function GetFormattedDate(date) {
+  var fields = date.split("/");
 
-    var day = fields[0];
-    var month = fields[1];
-    var year = fields[2];
+  var day = fields[0];
+  var month = fields[1];
+  var year = fields[2];
 
-    date = year + '-' + month + '-' + day;
-    return date;
+  date = year + "-" + month + "-" + day;
+  return date;
 }
 
-function GetQuery (Spending, callback) {
-    var currentDate = new Date();
-    Spending.aggregate([
-        { $project: { month: { $month: '$date' }, year: { $year: '$date' }, cost: true } },
-        { $match: { month: currentDate.getMonth() + 1, year: currentDate.getFullYear() } },
+function GetQuery(Spending, callback) {
+  var currentDate = new Date();
+  Spending.aggregate([
+    {
+      $project: {
+        month: { $month: "$date" },
+        year: { $year: "$date" },
+        cost: true,
+      },
+    },
+    {
+      $match: {
+        month: currentDate.getMonth() + 1,
+        year: currentDate.getFullYear(),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        total: { $sum: "$cost" },
+      },
+    },
+  ])
+    .exec()
+    .then((result) => {
+      if (result.length == 0) {
+        //If result is null set total amount to 0
+        result = [{ _id: null, total: 0 }];
+      }
+
+      Spending.aggregate([
         {
-            $group: {
-                _id: null,
-                total: { $sum: '$cost' }
-            }
-        }
-    ]).exec().then(result => {
-        if (result.length == 0) {
-            //If result is null set total amount to 0
-            result = [{ _id: null, total: 0 }];
-        }
-
-        Spending.aggregate([
-            { $match: { date: { $eq: new Date(Sugar.Date.format(new Date(), '%Y-%m-%d')) } } },
-            {
-                $group: {
-                    _id: "$_id",
-                    item: { $first: '$item' },
-                    cost: { $first: '$cost' },
-                    date: { $first: '$date' },
-                }
-            },
-            {
-                $project: {
-                    _id: true,
-                    item: true,
-                    cost: true,
-                    date: { $dateToString: { format: "%d/%m/%G", date: "$date" } }
-                }
-            }
-        ]).then(test => {
-            callback(result, test);
-        }).catch(err => {
-            console.log(err);
-            callback(err);
+          $match: {
+            date: { $eq: new Date(Sugar.Date.format(new Date(), "%Y-%m-%d")) },
+          },
+        },
+        {
+          $group: {
+            _id: "$_id",
+            item: { $first: "$item" },
+            cost: { $first: "$cost" },
+            date: { $first: "$date" },
+          },
+        },
+        {
+          $project: {
+            _id: true,
+            item: true,
+            cost: true,
+            date: { $dateToString: { format: "%d/%m/%G", date: "$date" } },
+          },
+        },
+      ])
+        .then((test) => {
+          callback(result, test);
+        })
+        .catch((err) => {
+          console.log(err);
+          callback(err);
         });
-
     })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 }
 
+function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
 
-function formatMoney (amount, decimalCount = 2, decimal = ".", thousands = ",") {
-    try {
-        decimalCount = Math.abs(decimalCount);
-        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+    const negativeSign = amount < 0 ? "-" : "";
 
-        const negativeSign = amount < 0 ? "-" : "";
+    let i = parseInt(
+      (amount = Math.abs(Number(amount) || 0).toFixed(decimalCount))
+    ).toString();
+    let j = i.length > 3 ? i.length % 3 : 0;
 
-        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
-        let j = (i.length > 3) ? i.length % 3 : 0;
-
-        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
-    } catch (e) {
-        console.log(e)
-    }
-};
+    return (
+      negativeSign +
+      (j ? i.substr(0, j) + thousands : "") +
+      i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) +
+      (decimalCount
+        ? decimal +
+          Math.abs(amount - i)
+            .toFixed(decimalCount)
+            .slice(2)
+        : "")
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
 module.exports = router;
