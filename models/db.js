93846eb803de308fa8e86a2b8bbeea6891223edf
ewyrudsbhsladk <8340935+adsnjhfyeqw231eas@users.noreply.github.com>
2023-08-28 20:47:18 +0530

    Update db.js
    passing DATABASE_URL value

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-
+var DATABASE_URL="mongodb+srv://shubham:shubham@cluster0.iolos.mongdb.net/staging?retryWrites=true&w=majority"
 const dbLink = process.env.DATABASE_URL;
 try {
     mongoose.connect(dbLink, { useUnifiedTopology: true, useNewUrlParser: true }, function (err) {
